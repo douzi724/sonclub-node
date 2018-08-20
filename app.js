06ef4669fc7a6b019d9e9562c5a5dc9438078176
nemo.zhong <zyj724@gmail.com>
2018-08-20 11:41:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,95 +1,370 @@
-/**
- * 主程序
- * @nemo_zhong
- */
-var express = require('express');
-var swig = require('swig');
-var config = require('./resources/config');
-var db = require('./src/helper/db_hlp');
-var routes = require('./src/controllers/routes');
+package tracing
 
-var app = express.createServer();
+import (
+	"context"
+	"net/http"
 
-console.log('app configure start...');
-// configuration in all env
-var static_dir = __dirname + '/public';
-app.configure(function() {
-  //init swig
-  app.register('.html', swig);
-  app.set('view engine', 'html');
-  swig.init({
-    root: __dirname + '/views',
-    autoescape: true,
-    encoding: 'utf8',
-    allowErrors: true // allows errors to be thrown and caught by express
-  });
-  app.set('views', __dirname + '/views');
-  app.set('view options', { layout: false });
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+)
 
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({
-    secret: config.system.session_secret
-  }));
+// SpanStart 开始一个http请求span
+func SpanStart(r *http.Request, tags map[string]string) opentracing.Span {
+	ctx, _ := Tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
+	sp := Tracer.StartSpan("HTTP "+r.Method+" "+r.URL.Path, ext.RPCServerOption(ctx))
+	if sp != nil {
+		for k, v := range tags {
+			sp.SetTag(k, v)
+		}
+		*r = *r.WithContext(opentracing.ContextWithSpan(r.Context(), sp))
+	}
 
-  // custom middleware
-  //app.use(require('./src/controllers/sys/sign_ctrl').signAuth);
-  app.use(require('./src/helper/req_hlp'));
-  app.use(express.csrf());
-});
+	return sp
+}
 
-//set static,dynamic helpers
-app.helpers({
-  config: config
-});
-app.dynamicHelpers({
-  csrf: function(req, res) {
-    return req.session ? req.session._csrf : '';
-  },
-  getFlash: function (req, res) {
-    return function(name) {
-      return req.flash(name);
-    }
-  },
-  flashMsg: function (req, res) {
-    return req.flash('flashMsg');
-  },
-  msgType: function (req, res) {
-    return req.flash('msgType');
-  }
-});
+// SpanStart 结束一个http请求span
+func SpanFinish(sp opentracing.Span, statusCode uint16) {
+	if sp != nil {
+		ext.HTTPStatusCode.Set(sp, statusCode)
+		sp.Finish()
+	}
+}
 
-app.configure('development', function() {
-  console.log('--> development environment');
-  app.use(express.static(static_dir));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  //require('./hot_deployer');
-});
+// ReqWithTracing 将请求加入当前追踪链中
+func ReqWithTracing(ctx *context.Context, r *http.Request) {
+	sp := opentracing.SpanFromContext(*ctx)
+	if sp != nil {
+		carrier := opentracing.HTTPHeadersCarrier(r.Header)
+		sp.Tracer().Inject(sp.Context(), opentracing.HTTPHeaders, carrier)
+	}
+}
 
-app.configure('production', function() {
-  console.log('--> production environment');
-  var one_year = 1000*60*60*24*365;
-  app.use(express.static(static_dir, { maxAge:one_year }));
-  app.use(express.errorHandler());
-  app.set('view cache', true);
-});
-console.log('app configure success!!!');
+// GetCurrentSpan 获取当前上下文中span
+func GetCurrentSpan(ctx *context.Context) opentracing.Span {
+	return opentracing.SpanFromContext(*ctx)
+}
 
-/*app.error(function(err, req, res, next){
- if (err instanceof NotFound) {
- res.render('404.html');
- } else {
- next(err);
- }
- });*/
+// CreateChildSpan 在当前上下文中创建一个子span
+func CreateChildSpan(ctx *context.Context, operationName string, tags map[string]string) (*context.Context, opentracing.Span) {
+	sp := GetCurrentSpan(ctx)
+	if sp != nil {
+		sp = sp.Tracer().StartSpan(operationName, opentracing.ChildOf(sp.Context()))
+		ext.SpanKindRPCClient.Set(sp)
+		ext.Component.Set(sp, "inner")
+		for k, v := range tags {
+			sp.SetTag(k, v)
+		}
+		ctxC := opentracing.ContextWithSpan(*ctx, sp)
+		return &ctxC, sp
+	}
 
-//db conn
-db.conn(config);
+	return ctx, sp
+}
 
-//bind controllers
-routes.bind(app);
+package tracing
 
-//startup
-app.listen(process.env.VMC_APP_PORT || config.system.port);
-console.log("Son Club listening on port %d in %s mode", app.address().port, app.settings.env);
+import (
+	"ESM-Monitor-Proxy/pkg/log"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-client-go/rpcmetrics"
+	"github.com/uber/jaeger-lib/metrics"
+	"github.com/uber/jaeger-lib/metrics/prometheus"
+	"go.uber.org/zap"
+)
+
+var (
+	logger         *zap.Logger
+	metricsFactory metrics.Factory
+	Tracer         *TracerLog
+)
+
+type TracerLog struct {
+	opentracing.Tracer
+	Log    tlog.Factory
+	Closer io.Closer
+}
+
+type TracerCfg struct {
+	ServiceName   string
+	AgentHostPort string
+	LogFile       string
+	LogDebug      bool
+	LogJson       bool
+}
+
+// InitTracer 初始化创建一个 Jaeger tracer 实例
+func InitTracer(c TracerCfg) *TracerLog {
+	initLogger(c)
+	f := tlog.NewFactory(logger.With(zap.String("service", c.ServiceName)))
+	cfg := config.Configuration{
+		ServiceName: c.ServiceName,
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans:            false,
+			BufferFlushInterval: 1 * time.Second,
+			LocalAgentHostPort:  c.AgentHostPort,
+		},
+	}
+
+	tracer, closer, err := cfg.NewTracer(
+		config.Logger(jaegerLoggerAdapter{f.Bg()}),
+		config.Observer(rpcmetrics.NewObserver(metricsFactory, rpcmetrics.DefaultNameNormalizer)),
+	)
+	if err != nil {
+		f.Bg().Fatal("初始化 Jaeger Tracer 实例出错", zap.Error(err))
+	}
+	Tracer = &TracerLog{Tracer: tracer, Log: f, Closer: closer}
+	return Tracer
+}
+
+func initLogger(c TracerCfg) {
+	if c.LogDebug {
+		logger, _ = zap.NewDevelopment()
+	} else {
+		cfg := zap.NewProductionConfig()
+		cfg.OutputPaths = []string{c.LogFile, "stderr"}
+		if !c.LogJson {
+			cfg.Encoding = "console"
+		}
+		logger, _ = cfg.Build()
+	}
+	metricsFactory = prometheus.New()
+	logger.Info("Using Prometheus as metrics backend")
+}
+
+type jaegerLoggerAdapter struct {
+	logger tlog.Logger
+}
+
+func (l jaegerLoggerAdapter) Error(msg string) {
+	l.logger.Error(msg)
+}
+
+func (l jaegerLoggerAdapter) Infof(msg string, args ...interface{}) {
+	l.logger.Info(fmt.Sprintf(msg, args...))
+}
+
+package tlog
+
+import (
+	"context"
+
+	"github.com/opentracing/opentracing-go"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Factory 日志封装
+type Factory struct {
+	logger *zap.Logger
+}
+
+// NewFactory 创建日志Factory
+func NewFactory(logger *zap.Logger) Factory {
+	return Factory{logger: logger}
+}
+
+// Bg 记录和上下文中span无关的日志
+func (b Factory) Bg() Logger {
+	return logger{logger: b.logger}
+}
+
+// For 记录和上下文中span有关的日志
+func (b Factory) For(ctx *context.Context) Logger {
+	if span := opentracing.SpanFromContext(*ctx); span != nil {
+		return spanLogger{span: span, logger: b.logger}
+	}
+	return b.Bg()
+}
+
+// With clone子日志记录，使其不影响父日志
+func (b Factory) With(fields ...zapcore.Field) Factory {
+	return Factory{logger: b.logger.With(fields...)}
+}
+
+package tlog
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Logger 各级别日志接口
+type Logger interface {
+	Info(msg string, fields ...zapcore.Field)
+	Error(msg string, fields ...zapcore.Field)
+	Debug(msg string, fields ...zapcore.Field)
+	Fatal(msg string, fields ...zapcore.Field)
+	With(fields ...zapcore.Field) Logger
+}
+
+type logger struct {
+	logger *zap.Logger
+}
+
+func (l logger) Info(msg string, fields ...zapcore.Field) {
+	l.logger.Info(msg, fields...)
+}
+
+func (l logger) Error(msg string, fields ...zapcore.Field) {
+	l.logger.Error(msg, fields...)
+}
+
+func (l logger) Debug(msg string, fields ...zapcore.Field) {
+	l.logger.Debug(msg, fields...)
+}
+
+func (l logger) Fatal(msg string, fields ...zapcore.Field) {
+	l.logger.Fatal(msg, fields...)
+}
+
+// With clone子日志记录，使其不影响父日志
+func (l logger) With(fields ...zapcore.Field) Logger {
+	return logger{logger: l.logger.With(fields...)}
+}
+
+package tlog
+
+import (
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/opentracing/opentracing-go/log"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type spanLogger struct {
+	logger *zap.Logger
+	span   opentracing.Span
+}
+
+func (sl spanLogger) Info(msg string, fields ...zapcore.Field) {
+	sl.logToSpan("info", msg, fields...)
+	sl.logger.Info(msg, fields...)
+}
+
+func (sl spanLogger) Error(msg string, fields ...zapcore.Field) {
+	ext.Error.Set(sl.span, true)
+	sl.logToSpan("error", msg, fields...)
+	sl.logger.Error(msg, fields...)
+}
+
+func (sl spanLogger) Debug(msg string, fields ...zapcore.Field) {
+	sl.logToSpan("debug", msg, fields...)
+	sl.logger.Debug(msg, fields...)
+}
+
+func (sl spanLogger) Fatal(msg string, fields ...zapcore.Field) {
+	sl.logToSpan("fatal", msg, fields...)
+	ext.Error.Set(sl.span, true)
+	sl.logger.Fatal(msg, fields...)
+}
+
+// With clone子日志记录，使其不影响父日志
+func (sl spanLogger) With(fields ...zapcore.Field) Logger {
+	return spanLogger{logger: sl.logger.With(fields...), span: sl.span}
+}
+
+func (sl spanLogger) logToSpan(level string, msg string, fields ...zapcore.Field) {
+	fa := fieldAdapter(make([]log.Field, 0, 2+len(fields)))
+	fa = append(fa, log.String("event", msg))
+	fa = append(fa, log.String("level", level))
+	for _, field := range fields {
+		field.AddTo(&fa)
+	}
+	sl.span.LogFields(fa...)
+}
+
+type fieldAdapter []log.Field
+
+func (fa *fieldAdapter) AddBool(key string, value bool) {
+	*fa = append(*fa, log.Bool(key, value))
+}
+
+func (fa *fieldAdapter) AddFloat64(key string, value float64) {
+	*fa = append(*fa, log.Float64(key, value))
+}
+
+func (fa *fieldAdapter) AddFloat32(key string, value float32) {
+	*fa = append(*fa, log.Float64(key, float64(value)))
+}
+
+func (fa *fieldAdapter) AddInt(key string, value int) {
+	*fa = append(*fa, log.Int(key, value))
+}
+
+func (fa *fieldAdapter) AddInt64(key string, value int64) {
+	*fa = append(*fa, log.Int64(key, value))
+}
+
+func (fa *fieldAdapter) AddInt32(key string, value int32) {
+	*fa = append(*fa, log.Int64(key, int64(value)))
+}
+
+func (fa *fieldAdapter) AddInt16(key string, value int16) {
+	*fa = append(*fa, log.Int64(key, int64(value)))
+}
+
+func (fa *fieldAdapter) AddInt8(key string, value int8) {
+	*fa = append(*fa, log.Int64(key, int64(value)))
+}
+
+func (fa *fieldAdapter) AddUint(key string, value uint) {
+	*fa = append(*fa, log.Uint64(key, uint64(value)))
+}
+
+func (fa *fieldAdapter) AddUint64(key string, value uint64) {
+	*fa = append(*fa, log.Uint64(key, value))
+}
+
+func (fa *fieldAdapter) AddUint32(key string, value uint32) {
+	*fa = append(*fa, log.Uint64(key, uint64(value)))
+}
+
+func (fa *fieldAdapter) AddUint16(key string, value uint16) {
+	*fa = append(*fa, log.Uint64(key, uint64(value)))
+}
+
+func (fa *fieldAdapter) AddUint8(key string, value uint8) {
+	*fa = append(*fa, log.Uint64(key, uint64(value)))
+}
+
+func (fa *fieldAdapter) AddUintptr(key string, value uintptr)                        {}
+func (fa *fieldAdapter) AddArray(key string, marshaler zapcore.ArrayMarshaler) error { return nil }
+func (fa *fieldAdapter) AddComplex128(key string, value complex128)                  {}
+func (fa *fieldAdapter) AddComplex64(key string, value complex64)                    {}
+func (fa *fieldAdapter) AddObject(key string, value zapcore.ObjectMarshaler) error   { return nil }
+func (fa *fieldAdapter) AddReflected(key string, value interface{}) error            { return nil }
+func (fa *fieldAdapter) OpenNamespace(key string)                                    {}
+
+func (fa *fieldAdapter) AddDuration(key string, value time.Duration) {
+	*fa = append(*fa, log.String(key, value.String()))
+}
+
+func (fa *fieldAdapter) AddTime(key string, value time.Time) {
+	*fa = append(*fa, log.String(key, value.String()))
+}
+
+func (fa *fieldAdapter) AddBinary(key string, value []byte) {
+	*fa = append(*fa, log.Object(key, value))
+}
+
+func (fa *fieldAdapter) AddByteString(key string, value []byte) {
+	*fa = append(*fa, log.Object(key, value))
+}
+
+func (fa *fieldAdapter) AddString(key, value string) {
+	if key != "" && value != "" {
+		*fa = append(*fa, log.String(key, value))
+	}
+}
